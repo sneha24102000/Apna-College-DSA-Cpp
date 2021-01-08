22e6002d2f8307e36c99272fa81afffe622904e2
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-01-09 01:04:27 +0530

    Add files via upload

---
+++ Recursionprob1.cpp
@@ -0,0 +1,23 @@
+// Recursion Problems
+//Add all Numbers till n
+
+#include<iostream>
+
+using namespace std;
+
+int Sum(int n)
+{
+	if(n==0){
+		return 0;
+	}
+	
+	int prevSum = Sum(n-1);
+	return n + prevSum;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<Sum(n)<<endl;
+}
