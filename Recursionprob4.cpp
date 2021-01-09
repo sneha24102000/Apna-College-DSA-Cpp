50625e0050833e515222cc62b89396f5c062e0e7
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-01-10 01:39:04 +0530

    Add files via upload

---
+++ Recursionprob4.cpp
@@ -0,0 +1,26 @@
+//Find the factorial of a number n! using recursion
+
+#include<iostream>
+
+using namespace std;
+
+int factorial(int n)
+{
+	if(n==0)
+	{
+		return 1;
+	}
+
+
+int prevfact = factorial(n-1);
+return n*prevfact;
+
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<factorial(n)<<endl;
+	return 0;
+}
