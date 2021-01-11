7181999daa059801b0dc083636166cfaaeb55c52
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-01-12 00:51:26 +0530

    Add files via upload

---
+++ Recursionprob6(2).cpp
@@ -0,0 +1,28 @@
+//Print numbers till n
+//1.Decreasing order
+//2.Increasing order
+
+#include<iostream>
+using namespace std;
+
+void inc(int n)
+{
+	if(n==0)
+	{
+		return;
+	}
+	
+	inc(n-1);
+	cout<<n<<endl;
+	
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	inc(n);
+	return 0;
+}
+
+
