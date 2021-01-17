cef32d7448494f3e2049d1a33d981e8c41be793b
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-01-18 01:41:53 +0530

    Add files via upload

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,102 +1,49 @@
-//Merge Sort
+//Insertion Sort
 
-// C++ program for Merge Sort
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-// Merges two subarrays of arr[].
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
-void merge(int arr[], int l, int m, int r)
+void swap(int arr[], int i , int j)
 {
-	int n1 = m - l + 1;
-	int n2 = r - m;
+	int temp=arr[i];
+	arr[i]=arr[j];
+	arr[j]=temp;
+}
 
-	// Create temp arrays
-	int L[n1], R[n2];
-
-	// Copy data to temp arrays L[] and R[]
-	for (int i = 0; i < n1; i++)
-		L[i] = arr[l + i];
-	for (int j = 0; j < n2; j++)
-		R[j] = arr[m + 1 + j];
-
-	// Merge the temp arrays back into arr[l..r]
-
-	// Initial index of first subarray
-	int i = 0;
-
-	// Initial index of second subarray
-	int j = 0;
-
-	// Initial index of merged subarray
-	int k = l;
-
-	while (i < n1 && j < n2) {
-		if (L[i] <= R[j]) {
-			arr[k] = L[i];
+int partition(int arr[], int l, int r)
+{
+	int pivot=arr[r];
+	int i=l-1;
+	
+	for(int j=l;j<r;j++){
+		if(arr[j]<pivot){
 			i++;
+			swap(arr,i,j);
 		}
-		else {
-			arr[k] = R[j];
-			j++;
-		}
-		k++;
 	}
-
-	// Copy the remaining elements of
-	// L[], if there are any
-	while (i < n1) {
-		arr[k] = L[i];
-		i++;
-		k++;
-	}
-
-	// Copy the remaining elements of
-	// R[], if there are any
-	while (j < n2) {
-		arr[k] = R[j];
-		j++;
-		k++;
-	}
+	
+	swap(arr,i+1,r);
+	return i+1;
 }
 
-// l is for left index and r is
-// right index of the sub-array
-// of arr to be sorted */
-void mergeSort(int arr[],int l,int r){
-	if(l>=r){
-		return;//returns recursively
-	}
-	int m = (l+r-1)/2;
-	mergeSort(arr,l,m);
-	mergeSort(arr,m+1,r);
-	merge(arr,l,m,r);
-}
-
-// UTILITY FUNCTIONS
-// Function to print an array
-void printArray(int A[], int size)
+void quickSort(int arr[], int l, int r)
 {
-	for (int i = 0; i < size; i++)
-		cout << A[i] << " ";
+	if(l<r)
+	{
+		int pi=partition(arr,l,r);
+		quickSort(arr,l,pi-1);
+		quickSort(arr,pi+1,r);
+		
+	}
 }
-
-// Driver code
-int main()
-{
-	int arr[] = { 12, 11, 13, 5, 6, 7 };
-	int arr_size = sizeof(arr) / sizeof(arr[0]);
-
-	cout << "Given array is \n";
-	printArray(arr, arr_size);
-
-	mergeSort(arr, 0, arr_size - 1);
-
-	cout << "\nSorted array is \n";
-	printArray(arr, arr_size);
-	return 0;
-}
-
-
-
+ int main()
+ 
+ {  
+    int arr[5]={5,4,3,2,1};
+    quickSort(arr,0,4);
+    for(int i=0;i<5;i++)
+    {
+    	cout<<arr[i]<<" ";
+	} cout<<endl;
+ 	return 0;
+ }
