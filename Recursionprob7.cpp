ee087529d0c33783353ae97ae14e410de1ec5d77
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-01-19 19:11:42 +0530

    Add files via upload

---
+++ Recursionprob7.cpp
@@ -0,0 +1,25 @@
+//Reverse a string using recursion
+
+//"binod"->"donib"
+
+#include<iostream>
+using namespace std;
+
+void reverse(string s){
+
+if(s.length()==0){
+return;
+	}
+	
+
+
+string ros=s.substr(1);
+reverse(ros);
+cout<<s[0];
+}
+
+int main()
+{
+	reverse("binod");
+	return 0;
+}
