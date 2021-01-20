449d1942ca702300dd7ea7b1eb90d6b5c3c00365
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-01-21 01:07:02 +0530

    Add files via upload

---
+++ RecursionProb12.cpp
@@ -0,0 +1,30 @@
+//Print all the possible words from phone digits
+
+#include<iostream>
+using namespace std;
+
+string keypadArr[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+void subseq(string s , string ans)
+{
+	if(s.length()==0)
+	{
+		cout<<ans<<endl;
+		return;
+	}
+	
+	char ch = s[0];
+	string code = keypadArr[ch-'0'];
+	string ros = s.substr(1);
+	
+	for(int i=0 ; i<code.length() ; i++)
+	{
+		keypadArr(ros, ans+code[i]);
+	}
+}
+
+int main()
+{
+	keypad("23","");
+	return 0;
+}
